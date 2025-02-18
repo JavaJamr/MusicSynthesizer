517d341e03623d1d9b3cbb094e0294ae64184f57
JavaJamr <gryan@oakland.edu>
2025-02-18 14:46:42 -0500

    This is needed for sound

--- Music_Synthesizer/src/main/java/module-info.java
+++ Music_Synthesizer/src/main/java/module-info.java
@@ -2,5 +2,6 @@ module com.mycompany.music_synthesizer {
     requires javafx.controls;
     requires javafx.base;
     requires javafx.graphics;
+        requires java.desktop;
     exports com.mycompany.music_synthesizer;
 }
